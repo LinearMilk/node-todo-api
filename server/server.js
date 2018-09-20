8435ee34f6d720c6197a3bff2d1fccc81362b97d
LinearMilk <witold.henszel@gmail.com>
2018-09-20 15:52:51 +0200

    Adding lodash

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var { mongoose } = require("./db/mongoose");
-var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
+const _ = require("lodash");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
 const { ObjectID } = require("mongodb");
 
